07ffa903ebe07e0d9d897b5897e5cdf709b64b52
aswin.sajeevan@qburst.com <aswin.sajeevan@qburst.com>
2023-12-05 17:26:48 +0530

    added postgresql

--- app/my_blog_site/settings.py
+++ app/my_blog_site/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'my_blog_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_site',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '5432', 
     }
 }
 
